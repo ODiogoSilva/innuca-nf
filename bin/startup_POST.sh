11d9e5b8b3e58cf7be0b1967a84675a7545a6f1e
ODiogoSilva <o.diogosilva@gmail.com>
2017-11-14 11:26:22 +0000

    Added status at startup

--- bin/startup_POST.sh
+++ bin/startup_POST.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-json="{'project_id':'$1','pipeline_id':'$2','process_id':'$3','run_property':'log_file','run_property_value':'$(pwd)/.command.log','type':'output'}"
+json="{'project_id':'$1','pipeline_id':'$2','process_id':'$3','run_property':'log_file,status','run_property_value':'$(pwd)/.command.log,running','type':'output'}"
+
 
 curl -H  "Content-Type: application/json" -L -X PUT -d \"$json\" $4
