3f6f3cc34ead8282483267d7ba502b8cd8e514e3
ODiogoSilva <o.diogosilva@gmail.com>
2017-11-14 11:16:53 +0000

    Fixed post request

--- bin/startup_POST.sh
+++ bin/startup_POST.sh
@@ -3,5 +3,5 @@
 json="{'project_id': '$1', 'pipeline_id': '$2', 'process_id': '$3', 'run_property': 'log_file', 'run_property_value': '$(pwd)/.command.log', 'type': 'output'}"
 
 curl -H  "Content-Type: application/json" \
-     -L -X PUT -d $json \
+     -L -X PUT -d \"$json\" \
      $4
