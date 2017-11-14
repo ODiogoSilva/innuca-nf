80e85f2063cb5e3c3e09edeb54d883dce82a230b
ODiogoSilva <o.diogosilva@gmail.com>
2017-11-14 11:22:41 +0000

    Fixed post request

--- bin/final_POST.sh
+++ bin/final_POST.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-json="{'project_id': '$1', 'pipeline_id': '$2', 'process_id': '$3', 'run_info': 'None', 'run_output': 'None, 'run_stats': 'None', 'log_file': '$(pwd)/.command.log', 'status': '$(pwd)/.status'}"
+json="{'project_id':'$1','pipeline_id':'$2','process_id':'$3','run_info':'None','run_output':'None,'run_stats':'None','log_file':'$(pwd)/.command.log','status':'$(pwd)/.status'}"
 
-curl -H  "Content-Type: application/json" \
-     -L -X POST -d \"$json\" \
-     $4
+curl -H  "Content-Type: application/json" -L -X POST -d \"$json\" $4
