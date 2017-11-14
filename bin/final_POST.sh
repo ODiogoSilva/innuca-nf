7a68a86ed3ed9d2f51381b69c4bfcfcae451166e
ODiogoSilva <o.diogosilva@gmail.com>
2017-11-14 11:42:32 +0000

    Added status as a string in post request

--- bin/final_POST.sh
+++ bin/final_POST.sh
@@ -1,5 +1,7 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-json="{'project_id':'$1','pipeline_id':'$2','process_id':'$3','run_info':'None','run_output':'None,'run_stats':'None','log_file':'$(pwd)/.command.log','status':'$(pwd)/.status'}"
+st=`cat $(pwd)/.status`
+
+json="{'project_id':'$1','pipeline_id':'$2','process_id':'$3','run_info':'None','run_output':'None','run_stats':'None','log_file':'$(pwd)/.command.log','status':'$st'}"
 
 curl -H  "Content-Type: application/json" -L -X POST -d \"$json\" $4
