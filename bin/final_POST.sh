fb95bf9055496ad646d794aebe94abf20190cb40
ODiogoSilva <o.diogosilva@gmail.com>
2017-11-17 20:16:15 +0000

    Silenced request output for logging file

--- bin/final_POST.sh
+++ bin/final_POST.sh
@@ -4,4 +4,4 @@ st=`cat $(pwd)/.status`
 
 json="{'project_id':'$1','pipeline_id':'$2','process_id':'$3','run_info':'None','run_output':'None','run_stats':'None','log_file':'$(pwd)/.command.log','status':'$st','type':'output'}"
 
-curl -H  "Content-Type: application/json" -L -X POST -d \"$json\" $4
+curl -H  "Content-Type: application/json" -L -X POST -d \"$json\" $4 > /dev/null
