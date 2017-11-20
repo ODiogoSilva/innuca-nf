a71df36ae39558cc96fcfa44dd04ca30fb714777
ODiogoSilva <o.diogosilva@gmail.com>
2017-11-20 10:38:26 +0000

    Modified post to include warning files

--- bin/final_POST.sh
+++ bin/final_POST.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
-st=`cat $(pwd)/.status`
+st=$(cat $(pwd)/.status)
+warn=$(cat $(pwd)/.warnings)
 
-json="{'project_id':'$1','pipeline_id':'$2','process_id':'$3','run_info':'None','run_output':'None','run_stats':'None','log_file':'$(pwd)/.command.log','status':'$st','type':'output'}"
+json="{'project_id':'$1','pipeline_id':'$2','process_id':'$3','run_info':'None','run_output':'None','warnings':'$warn','log_file':'$(pwd)/.command.log','status':'$st','type':'output'}"
 
 curl -H  "Content-Type: application/json" -L -X POST -d \"$json\" $4
