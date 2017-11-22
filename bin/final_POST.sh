a156a5f5ee11691fecd3a139f910b4874ce3a730
ODiogoSilva <o.diogosilva@gmail.com>
2017-11-22 15:07:36 +0000

    Fixed issues with post/put requests

--- bin/final_POST.sh
+++ bin/final_POST.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env sh
 
 st=$(cat $(pwd)/.status)
-warn=$(cat $(pwd)/.warning)
 
-## TODO: Change run_info to fails
-
-json="{'project_id':'$1','pipeline_id':'$2','process_id':'$3','run_info':'None','run_output':'None','warnings':'$warn','log_file':'$(pwd)/.command.log','status':'$st','type':'output'}"
+json="{'project_id':'$1','pipeline_id':'$2','process_id':'$3','run_info':'None','run_output':'None','warnings':'$(pwd)/.warning','log_file':'$(pwd)/.command.log','status':'$st','type':'output'}"
 
 curl -H  "Content-Type: application/json" -L -X POST -d \"$json\" $4 > /dev/null
