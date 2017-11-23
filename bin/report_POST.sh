db4f99b04a55e6f724c0a839dbd50d2a7a6a2bf2
ODiogoSilva <o.diogosilva@gmail.com>
2017-11-23 19:07:27 +0000

    Post json str instead of path

--- bin/report_POST.sh
+++ bin/report_POST.sh
@@ -12,6 +12,7 @@ userid=$7
 if [ -s .report.json ];
 then
     json_str=$(cat $(pwd)/.report.json)
+    echo json_str
     json="{'project_id':'$projectid','pipeline_id':'$pipelineid','process_id':'$processid','sample_name':'$sample','report_json':'$json_str','current_user_name':'$username','current_user_id':'$userid'}"
     curl -H  "Content-Type: application/json" -k -L -X POST -d \"$json\" $url > /dev/null
 fi
