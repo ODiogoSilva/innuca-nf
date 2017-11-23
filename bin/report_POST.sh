ca22882c9aeec036e1015a7be40ca29690da54a7
ODiogoSilva <o.diogosilva@gmail.com>
2017-11-23 15:44:49 +0000

    Added json information for report post

--- bin/report_POST.sh
+++ bin/report_POST.sh
@@ -5,10 +5,12 @@ pipelineid=$2
 processid=$3
 sample=$4
 url=$5
+username=$6
+userid=$7
 
 
 if [ -s .report.json ];
 then
-    json="{'project_id':'$projectid','pipeline_id':'$pipelineid','process_id':'$processid','sample_name':'$sample','report_json':'.report.json'}"
+    json="{'project_id':'$projectid','pipeline_id':'$pipelineid','process_id':'$processid','sample_name':'$sample','report_json':'.report.json','current_user_name':'$username','current_user_id':'$userid'}"
     curl -H  "Content-Type: application/json" -k -L -X POST -d \"$json\" $url > /dev/null
 fi
