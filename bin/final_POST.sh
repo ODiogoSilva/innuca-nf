cb419d14abad96e29e0714a20979144b5428a675
ODiogoSilva <o.diogosilva@gmail.com>
2017-11-23 18:53:31 +0000

    Handled request exceptions

--- bin/final_POST.sh
+++ bin/final_POST.sh
@@ -4,4 +4,8 @@ st=$(cat $(pwd)/.status)
 
 json="{'project_id':'$1','pipeline_id':'$2','process_id':'$3','run_info':'None','run_output':'None','warnings':'$(pwd)/.warning','log_file':'$(pwd)/.command.log','status':'$st','type':'output'}"
 
-curl -H  "Content-Type: application/json" -L -X POST -d \"$json\" $4 > /dev/null
+{
+    curl -H  "Content-Type: application/json" -L -X POST -d \"$json\" $4 > /dev/null
+} || {
+    echo Curl request failed
+}
--- bin/startup_POST.sh
+++ bin/startup_POST.sh
@@ -2,4 +2,8 @@
 
 json="{'project_id':'$1','pipeline_id':'$2','process_id':'$3','run_property':'log_file,status','run_property_value':'$(pwd)/.command.log,running','type':'output'}"
 
-curl -H  "Content-Type: application/json" -L -X PUT -d \"$json\" $4 > /dev/null
+{
+    curl -H  "Content-Type: application/json" -L -X PUT -d \"$json\" $4 > /dev/null
+} || {
+    echo Curl request failed
+}
