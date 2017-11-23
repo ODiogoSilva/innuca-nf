70da8b24a6136194f0fbbb7586cf490e825f74b9
ODiogoSilva <o.diogosilva@gmail.com>
2017-11-23 19:00:15 +0000

    Fixed post request

--- bin/report_POST.sh
+++ bin/report_POST.sh
@@ -11,6 +11,6 @@ userid=$7
 
 if [ -s .report.json ];
 then
-    json="{'project_id':'$projectid','pipeline_id':'$pipelineid','process_id':'$processid','sample_name':'$sample','report_json':'.report.json','current_user_name':'$username','current_user_id':'$userid'}"
+    json="{'project_id':'$projectid','pipeline_id':'$pipelineid','process_id':'$processid','sample_name':'$sample','report_json':'$(pwd)/.report.json','current_user_name':'$username','current_user_id':'$userid'}"
     curl -H  "Content-Type: application/json" -k -L -X POST -d \"$json\" $url > /dev/null
 fi
