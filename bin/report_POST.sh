40066f319aa5ef7e996ba4c53ff2c0d737a8f55f
ODiogoSilva <o.diogosilva@gmail.com>
2017-11-23 15:08:55 +0000

    Fixed report post

--- bin/report_POST.sh
+++ bin/report_POST.sh
@@ -4,12 +4,11 @@ projectid=$1
 pipelineid=$2
 processid=$3
 sample=$4
-reportjson=$5
-url=$6
+url=$5
 
-json="{'project_id':'$projectid','pipeline_id':'$pipelineid','process_id':'$processid', 'sample_name': '$sample', 'report_json': '$reportjson'}"
 
 if [ -s .report.json ];
 then
+    json="{'project_id':'$projectid','pipeline_id':'$pipelineid','process_id':'$processid', 'sample_name': '$sample', 'report_json': '.report.json'}"
     curl -H  "Content-Type: application/json" -L -X POST -d \"$json\" $url > /dev/null
 fi
