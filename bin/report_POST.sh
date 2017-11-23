713224b7e07a831762899c38f334ae275ddcf05c
ODiogoSilva <o.diogosilva@gmail.com>
2017-11-23 19:10:29 +0000

    Fixed spacing in json

--- bin/report_POST.sh
+++ bin/report_POST.sh
@@ -11,8 +11,8 @@ userid=$7
 
 if [ -s .report.json ];
 then
-    json_str=$(cat $(pwd)/.report.json)
-    echo json_str
+    json_str=$(cat $(pwd)/.report.json | sed 's/ //g')
+    echo $json_str
     json="{'project_id':'$projectid','pipeline_id':'$pipelineid','process_id':'$processid','sample_name':'$sample','report_json':'$json_str','current_user_name':'$username','current_user_id':'$userid'}"
     curl -H  "Content-Type: application/json" -k -L -X POST -d \"$json\" $url > /dev/null
 fi
