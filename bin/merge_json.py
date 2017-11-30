df4bf4085a1970702ee64475af095e2893353f78
ODiogoSilva <o.diogosilva@gmail.com>
2017-11-30 18:23:49 +0000

    Fixed json merger

--- bin/merge_json.py
+++ bin/merge_json.py
@@ -10,7 +10,7 @@ with open(f1) as f1h, open(f2) as f2h:
     j1 = json.load(f1h)
     j2 = json.load(f2h)
 
-    l = [j1, j2]
+    l = {"cagao": [j1, j2]}
 
     with open(".report.json", "w") as fh:
         fh.write(json.dumps(l))
