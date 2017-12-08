aa615e2797259ceb7c7f419674c4a2968666d2b8
ODiogoSilva <o.diogosilva@gmail.com>
2017-12-08 14:54:52 +0000

    Fixed json key

--- bin/report_POST.sh
+++ bin/report_POST.sh
@@ -24,7 +24,7 @@ then
     fi
 
     workdir=$(pwd)
-    json="{'project_id':'$projectid','pipeline_id':'$pipelineid','process_id':'$processid','sample_name':'$sample','report_json':$json_str,'current_user_name':'$username','current_user_id':'$userid','workdir':'$workdir','task':'$task','current_specie':'$species'}"
+    json="{'project_id':'$projectid','pipeline_id':'$pipelineid','process_id':'$processid','sample_name':'$sample','report_json':$json_str,'current_user_name':'$username','current_user_id':'$userid','workdir':'$workdir','task':'$task','species':'$species'}"
     echo \"${json}\" > .final.json
     cat .final.json | curl -H  "Content-Type: application/json" -k -L -X POST -d @- $url > /dev/null
 fi
