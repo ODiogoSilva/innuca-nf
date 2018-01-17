5e23bf31922cf28e4a6806be241e34d5da8bca63
ODiogoSilva <o.diogosilva@gmail.com>
2018-01-17 13:00:31 +0000

    Modified report request to include a versions JSON

--- bin/report_POST.sh
+++ bin/report_POST.sh
@@ -12,17 +12,32 @@ userid=$7
 task=$8
 species=$9
 
+json_str=""
+version_str=""
 
+# If a .report.json file was populated, set the json_str variable
 if [ -s .report.json ];
 then
 
+    # Modification of the JSON string should be different for chewbbaca
+    # output
     if [ $task = "chewbbaca" ];
     then
         json_str=$(cat $(pwd)/.report.json | sed 's/ //g' | sed s/\"/\'/g)
     else
         json_str=$(cat $(pwd)/.report.json | sed 's/ /_/g' | sed s/\"/\'/g)
     fi
+fi
 
+# If a .versions file was populated, set the version_str variable
+if [ -s .versions ];
+then
+    version_str=$(cat $(pwd).versions | sed 's/ /_/g' | sed s/\"/\'/g)
+fi
+
+# If a .versions OR .report.json file was populated send the request
+if [ ! "$json_str" = "" ] || [ ! "version_str" = "" ];
+then
     workdir=$(pwd)
     json="{'project_id':'$projectid','pipeline_id':'$pipelineid','process_id':'$processid','sample_name':'$sample','report_json':$json_str,'current_user_name':'$username','current_user_id':'$userid','workdir':'$workdir','task':'$task','species':'$species'}"
     echo \"${json}\" > .final.json
